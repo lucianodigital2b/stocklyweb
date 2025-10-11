8aeb4c0a9c972a803fe87fe8ef45645a0444587b
Luciano <luciano.victor101@outlook.com>
2025-10-11 18:12:44 -0300

    fix

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -13,25 +13,9 @@ const router = createRouter({
 router.beforeEach(loadLayoutMiddleware)
 
 async function loadLayoutMiddleware(route, from) {
-
-    
-    try {
-        let layout = route.meta.layout;
-        
-        // let layoutComponent = await import(`/resources/js/layouts/${layout}.vue`)
-        let layoutComponent = await import(`/resources/js/layout/AppLayout.vue`)
-
-        route.meta.layoutComponent = layoutComponent.default
-    } catch (e) {
-        console.error('Error occurred in processing of layouts: ', e)
-        let layout = 'basic'
-        let layoutComponent = await import(`../layouts/${layout}.vue`)
-        
-        route.meta.layoutComponent = layoutComponent.default
-    }
-
-
-
+    let layout = route.meta.layout;
+    let layoutComponent = await import(`../layout/${layout}.vue`)
+    route.meta.layoutComponent = layoutComponent.default
 }
 
 
